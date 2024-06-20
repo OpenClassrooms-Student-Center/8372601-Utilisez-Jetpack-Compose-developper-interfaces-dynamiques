e2af6d3f7eedb3efbf9547fd7344864620fba3cf
DEMEY Fanny <fanny.pluvinage@gmail.com>
2024-06-20 14:26:18 +0200

    partie 2 chapitre 1

--- app/src/main/java/com/opc/bestpodcast/ui/PodcastItem.kt
+++ app/src/main/java/com/opc/bestpodcast/ui/PodcastItem.kt
@@ -1,16 +1,24 @@
 package com.opc.bestpodcast.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.DownloadDone
 import androidx.compose.material.icons.filled.Downloading
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.opc.bestpodcast.data.factory.PodcastFactory
 import com.opc.bestpodcast.data.model.DownloadStatus
 import com.opc.bestpodcast.data.model.Podcast
@@ -31,17 +39,29 @@ fun PodcastItem(
                     Icon(
                         imageVector = Icons.Default.Download,
                         contentDescription = "Télécharger ${podcast.title}",
+                        modifier =
+                            Modifier
+                                .clip(CircleShape)
+                                .background(Color.Yellow)
+                                .border(
+                                    width = 2.dp,
+                                    color = MaterialTheme.colorScheme.secondary,
+                                    shape = CircleShape,
+                                )
+                                .padding(4.dp),
                     )
                 }
             DownloadStatus.InProgress ->
                 Icon(
                     imageVector = Icons.Default.Downloading,
                     contentDescription = "Téléchargement en cours",
+                    modifier = Modifier.padding(12.dp),
                 )
             DownloadStatus.Downloaded ->
                 Icon(
                     imageVector = Icons.Default.DownloadDone,
                     contentDescription = "Téléchargé",
+                    modifier = Modifier.padding(12.dp),
                 )
         }
     }
@@ -65,9 +85,18 @@ fun PreviewPodcastItemInProgress() {
     )
 }
 
-@Preview(fontScale = 2.0f)
+@Preview
 @Composable
 fun PreviewPodcastItemDownloaded() {
+    PodcastItem(
+        podcast = PodcastFactory.makePodcasts()[0].copy(downloadStatus = DownloadStatus.Downloaded),
+        onDownloadClicked = {},
+    )
+}
+
+@Preview(fontScale = 2.0f)
+@Composable
+fun PreviewPodcastItemDownloadedLarge() {
     PodcastItem(
         podcast = PodcastFactory.makePodcasts()[0].copy(downloadStatus = DownloadStatus.Downloaded),
         onDownloadClicked = {},
